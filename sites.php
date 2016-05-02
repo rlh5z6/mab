4fca0a34310bd61cb7f11dd5258853d3c569197e
rlh5z6 <rlh5z6@mail.missouri.edu>
2016-05-02 12:40:12 -0500

    Add criteria for searching on the sites page

--- sites.php
+++ sites.php
@@ -50,12 +50,12 @@
       </div>
     <select class="form-control select select-primary" name="search" data-toggle="select">
         <option default>Search By</option>
-        <option value="Trip Name">Trip Name</option>
         <option value="City">City</option>
         <option value="State">State</option>
         <option value="Service Focus">Service Focus</option>
     </select> 
     </form>   
+    
     <?php
 	//Check to make sure the Search button is pushed
 	if(isset($_POST['searchButton']))
@@ -77,12 +77,35 @@
 
 		$searchText = $_POST['searchTxt'];
         $searchText = strtoupper($searchText);
+        $search = $_POST['search'];
+        
         
         $searchTextTemp = $searchText . '%';
-        $stmt = $conn->prepare("SELECT t.tripID, t.tripName, t.season, t.year, t.city, t.state, t.focus, h.housingID, h.siteName
+        
+        if ($search == 'City') {
+            $stmt = $conn->prepare("SELECT t.tripID, t.tripName, t.season, t.year, t.city, t.state, t.focus, h.housingID, h.siteName
                                 FROM trip t, housing h
                                 WHERE t.housingID = h.housingID
                                 AND t.city LIKE ?");
+        }
+        else if ($search == 'State') {
+            $stmt = $conn->prepare("SELECT t.tripID, t.tripName, t.season, t.year, t.city, t.state, t.focus, h.housingID, h.siteName
+                                FROM trip t, housing h
+                                WHERE t.housingID = h.housingID
+                                AND t.state LIKE ?");
+        }
+        else if ($search == 'Service Focus') {
+            $stmt = $conn->prepare("SELECT t.tripID, t.tripName, t.season, t.year, t.city, t.state, t.focus, h.housingID, h.siteName
+                                FROM trip t, housing h
+                                WHERE t.housingID = h.housingID
+                                AND t.focus LIKE ?");
+        }
+        else {
+            $stmt = $conn->prepare("SELECT t.tripID, t.tripName, t.season, t.year, t.city, t.state, t.focus, h.housingID, h.siteName
+                                FROM trip t, housing h
+                                WHERE t.housingID = h.housingID
+                                AND t.city LIKE ?");
+        }
         $stmt->bind_param("s", $searchTextTemp);
         $stmt->execute();
         $stmt->bind_result($tripID, $tripName, $season, $year, $city, $state, $focus, $housingID, $siteName);
